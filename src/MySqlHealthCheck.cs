bfd166353dcd2806c569e4aa8b64cee99beee7b9
Nguyễn Đạt <datnlt89@gmail.com>
2023-01-07 18:55:34 +0700

    Implement the MySqlHeathCheck

--- src/MySqlHealthCheck.cs
+++ src/MySqlHealthCheck.cs
@@ -1,5 +1,36 @@
-﻿namespace Ongdat.Diagnostics.HealthChecks.MySql;
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MySqlConnector;
 
-public class MySqlHealthCheck
+namespace Ongdat.Diagnostics.HealthChecks.MySql;
+
+public class MySqlHealthCheck : IHealthCheck
 {
+  private readonly string connectionString;
+
+  public MySqlHealthCheck(string connectionString)
+  {
+    if (string.IsNullOrEmpty(connectionString))
+    {
+      throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or empty.", nameof(connectionString));
+    }
+
+    this.connectionString = connectionString;
+  }
+
+  public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+  {
+    try
+    {
+      using var connection = new MySqlConnection(connectionString);
+      await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+      return await connection.PingAsync(cancellationToken).ConfigureAwait(false)
+        ? HealthCheckResult.Healthy()
+        : new HealthCheckResult(context.Registration.FailureStatus);
+    }
+    catch (Exception exception)
+    {
+      return new HealthCheckResult(context.Registration.FailureStatus, exception: exception);
+    }
+  }
 }
